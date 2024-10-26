eb9b6a07f597e730cbc563c4a9a21251fe750509
arlofonseca <lj6976294@mail.com>
2024-10-26 17:59:00 -0400

    refactor: no need to pre-install packages

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,43 +2,10 @@ return {
   {
     'williamboman/mason.nvim',
     cmd = 'Mason',
-    keys = { { '<leader>cm', '<cmd>Mason<cr>' } },
-    build = ':MasonUpdate',
-    opts = {
-      ensure_installed = {
-        'typescript-language-server',
-        'lua-language-server',
-        'prettierd',
-        'stylua',
-      },
-    },
+    keys = { { '<leader>pl', '<cmd>Mason<cr>' } },
+    opts = {},
     config = function(_, opts)
       require('mason').setup(opts)
-      local registry = require('mason-registry')
-
-      registry:on('success', function()
-        vim.defer_fn(function()
-          require('lazy.core.handler.event').trigger({
-            event = 'FileType',
-            buf = vim.api.nvim_get_current_buf(),
-          })
-        end, 100)
-      end)
-
-      local function installPackages()
-        for _, tool in ipairs(opts.ensure_installed) do
-          local package = registry.get_package(tool)
-          if not package:is_installed() then
-            package:install()
-          end
-        end
-      end
-
-      if registry.refresh then
-        registry.refresh(installPackages)
-      else
-        installPackages()
-      end
-    end,
+    end
   }
 }
