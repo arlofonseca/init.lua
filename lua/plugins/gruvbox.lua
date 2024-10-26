2b585f59f9077f2ff980ac79f7ca37686746da64
arlofonseca <lj6976294@mail.com>
2024-10-26 16:30:07 -0400

    change background

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,10 +1,17 @@
 return {
   'ellisonleao/gruvbox.nvim',
   config = function()
+    vim.keymap.set('n', '<leader>cb', function()
+      vim.o.background = vim.o.background == 'light' and 'dark' or 'light'
+      vim.cmd('hi NonText guifg=bg')
+    end)
+
     require('gruvbox').setup({
-      transparent_mode = true,
+      contrast = 'hard',
     })
 
-    vim.cmd('colorscheme gruvbox')
-  end,
+    vim.o.background = 'dark'
+    vim.cmd.colorscheme 'gruvbox'
+    vim.cmd('hi NonText guifg=bg')
+  end
 }
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,7 +1,7 @@
 return {
   {
+    'nvim-tree/nvim-web-devicons',
     'akinsho/toggleterm.nvim',
     'pantharshit00/vim-prisma',
-    'nvim-tree/nvim-web-devicons',
   }
 }
