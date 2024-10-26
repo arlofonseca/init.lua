13761b126632babae1fbfa559bf7e996e20e7b54
arlofonseca <lj6976294@mail.com>
2024-10-26 17:44:34 -0400

    mason

--- lua/config.lua
+++ lua/config.lua
@@ -7,24 +7,19 @@ vim.wo.relativenumber = true
 vim.o.lazyredraw = true
 vim.o.scrolloff = 8
 
-vim.keymap.set('n', '<leader>ls', vim.cmd.Ex)
+vim.keymap.set('n', 'u', '<Nop>')
+vim.keymap.set('n', 'p', '<Nop>')
+vim.keymap.set('n', '<Space>', '<Nop>')
 vim.keymap.set('n', '<C-f>', '/<C-r><C-w><CR>')
 vim.keymap.set('n', '<C-s>', ':w<CR>')
 vim.keymap.set('n', '<C-q>', ':qa!<CR>')
 vim.keymap.set('n', '<C-z>', 'u')
-vim.keymap.set('n', '<C-y>', '<C-r>')
 vim.keymap.set({ 'n', 'v' }, '<C-c>', '\'+y')
-vim.keymap.set('n', '<leader>bn', ':bnext<CR>')
-vim.keymap.set('n', '<leader>bp', ':bprevious<CR>')
-vim.keymap.set('n', 'u', '<Nop>')
-vim.keymap.set('n', 'p', '<Nop>')
-vim.keymap.set('n', '<Space>', '<Nop>')
-vim.cmd('autocmd BufEnter * normal! m`')
-vim.keymap.set('n', '<Esc>', ':b#<CR>')
-
-vim.keymap.set('n', '<leader>term', function()
-  require('toggleterm').toggle()
-end)
+vim.keymap.set('n', '<leader>ls', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>ff', ':find <C-R>=expand("%:h") . "/"<CR>')
+vim.keymap.set('n', '<leader>gb', ':bprev<CR>')
+vim.keymap.set('n', '<leader>gf', ':bnext<CR>')
+vim.keymap.set('n', '<leader>term', function() require('toggleterm').toggle() end)
 
 vim.cmd('set cursorline')
 vim.cmd('set cursorcolumn')
