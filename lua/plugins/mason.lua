7f339e033246993fee0c025815d0fcec11d9b931
arlofonseca <lj6976294@mail.com>
2024-10-26 18:05:45 -0400

    Revert eb9b6a0 lol

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,9 +3,41 @@ return {
     'williamboman/mason.nvim',
     cmd = 'Mason',
     keys = { { '<leader>pl', '<cmd>Mason<cr>' } },
-    opts = {},
+    opts = {
+      install = {
+        'typescript-language-server',
+        'lua-language-server',
+        'prettierd',
+        'stylua',
+      },
+    },
     config = function(_, opts)
       require('mason').setup(opts)
-    end
+      local registry = require('mason-registry')
+
+      registry:on('success', function()
+        vim.defer_fn(function()
+          require('lazy.core.handler.event').trigger({
+            event = 'FileType',
+            buf = vim.api.nvim_get_current_buf(),
+          })
+        end, 100)
+      end)
+
+      local function installPackages()
+        for _, tool in ipairs(opts.install) do
+          local package = registry.get_package(tool)
+          if not package:is_installed() then
+            package:install()
+          end
+        end
+      end
+
+      if registry.refresh then
+        registry.refresh(installPackages)
+      else
+        installPackages()
+      end
+    end,
   }
 }
