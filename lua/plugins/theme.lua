5271a491aa5d0c7430d390ebb5b903744fcc7509
arlofonseca <lj6976294@mail.com>
2024-10-27 04:25:42 -0400

    monokai

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -15,5 +15,20 @@ return {
       vim.cmd.colorscheme('gruvbox')
       vim.cmd('hi NonText guifg=bg')
     end
+  },
+  --[[
+  {
+    'polirritmico/monokai-nightasty.nvim',
+    config = function()
+      vim.keymap.set('n', '<leader>cb', function()
+        vim.o.background = vim.o.background == 'light' and 'dark' or 'light'
+        vim.cmd('hi NonText guifg=bg')
+      end)
+
+      vim.o.background = 'dark'
+      vim.cmd.colorscheme('monokai-nightasty')
+      vim.cmd('hi NonText guifg=bg')
+    end
   }
+  --]]
 }
