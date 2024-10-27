ba0fed5a6134efff6f8f9c524145df31c490ced4
arlofonseca <lj6976294@mail.com>
2024-10-27 04:08:16 -0400

    tweak: loading lazy

--- init.lua
+++ init.lua
@@ -1,14 +1,22 @@
 _G.vim = vim
 
-local path = ('%s/lazy/lazy.nvim'):format(vim.fn.stdpath('data'))
+local data_path = vim.fn.stdpath('data')
+local lazy_path = ('%s/lazy/lazy.nvim'):format(data_path)
 
-if not vim.loop.fs_stat(path) then
-  vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', path})
+if not vim.loop.fs_stat(lazy_path) then
+  vim.fn.system({ 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazy_path })
 end
 
-vim.opt.rtp:prepend(path)
+vim.opt.rtp:prepend(lazy_path)
 
-require('config/keymap')
-require('config/options')
+local function load(module)
+  local success, error = pcall(require, module)
+  if not success then
+    vim.notify(('Something went wrong while trying to load %s: %s'):format(module, error), vim.log.levels.ERROR)
+  end
+end
+
+load('config.keymap')
+load('config.options')
 
 require('lazy').setup('plugins')
